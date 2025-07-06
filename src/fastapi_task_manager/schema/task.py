49055372d9d9062144a5fae318ae82c43b38baaf
Matteo Morando <matteo.morando98@gmail.com>
2025-07-06 20:03:00 +0400

    task add task base and repr function

--- src/fastapi_task_manager/schema/task.py
+++ src/fastapi_task_manager/schema/task.py
@@ -3,7 +3,15 @@ from collections.abc import Callable
 from pydantic import BaseModel
 
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
+    expression: str
+    name: str
+    description: str | None = None
+    tags: list[str] | None = None
+    high_priority: bool = False
+
+
+class Task(TaskBase):
     """Schema for a task in the task manager."""
 
     model_config = {
@@ -11,11 +19,6 @@ class Task(BaseModel):
     }
 
     function: Callable
-    expression: str
-    name: str
-    description: str | None = None
-    tags: list[str] | None = None
-    high_priority: bool = False
 
     def __hash__(self):
         """Hash the task based on its expression and function."""
@@ -29,4 +32,13 @@ class Task(BaseModel):
             + str(self.tags or [])
             + "_"
             + self.function.__name__,
+        )
+
+    def __repr__(self):
+        return TaskBase(
+            expression=self.expression,
+            name=self.name,
+            description=self.description,
+            tags=self.tags,
+            high_priority=self.high_priority,
         )
