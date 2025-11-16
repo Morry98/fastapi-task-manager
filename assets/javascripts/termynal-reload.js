156a9adc20be36704944fa2c755e915051df982d
Matteo Morando <matteo.morando98@gmail.com>
2025-11-16 23:02:27 +0400

    Deployed c4acefb with MkDocs version: 1.6.1

--- assets/javascripts/termynal-reload.js
+++ assets/javascripts/termynal-reload.js
@@ -1,5 +1,13 @@
+// Flag to skip the first execution because is managed by termynal itself
+let isFirstLoad = true;
+
 // Re-initialize termynal after each page load
 document$.subscribe(function() {
+    if (isFirstLoad) {
+        isFirstLoad = false;
+        return;
+    }
+
     // Wait for the DOM to be updated
     setTimeout(function() {
         // Find all termynal blocks and re-initialize them
